0b64d55b8de3a23eb746af05e880645be3accb26
Student <sherpatashi50@gmail.com>
2022-11-22 11:43:11 -0500

    Added links, images, scripts

--- scripts/mountain.js
+++ scripts/mountain.js
@@ -6,7 +6,7 @@ let option = new Option("Select a mountain", "select");
 mountainsList.appendChild(option);
 
 window.onload = function() {
-    initMountainsList();
+    initMountainsList(); 
     mountainsList.onchange = displayInfo;
 }
 
@@ -24,8 +24,27 @@ function displayInfo() {
             mountainOptions.innerHTML = "";
         }
         else if (mountain.name == mountainsList.value) {
-            mountainOptions.innerHTML += "<br>" + mountain.desc + "<br><img src='images/" + mountain.img + "' width='100%'></img>";
-        }
+            mountainOptions.innerHTML += "<br>" +
+            mountain.name + "<br>" 
+            + "Elevation: "  + mountain.elevation + "<br>" + 
+            mountain.desc + "<br><img src='images/" + mountain.img +
+             "' width='100%'></img>";  
+        getSunsetForMountain(mountain.coords.lat, mountain.coords.lng).then(data => {
+                mountainOptions.innerHTML += "<br>" + "Sunrise/sunset (UTC)" + 
+                data.results.sunrise + "/" + data.results.sunset;
+               });
+            }
     }
 }
 
+
+// function that can "fetch" the sunrise/sunset times
+async function getSunsetForMountain(lat, lng){
+    let response = await fetch(
+    `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&date=today`);
+    let data = await response.json();
+    return data;
+   }
+
+// Fetch the sunset/sunrise times for a specific mountain
+   
--- scripts/park.js
+++ scripts/park.js
@@ -4,23 +4,25 @@ const filterOptionsList = document.getElementById("filterOptionsList");
 const locationsList = document.getElementById("locationsList");
 const parkTypeList = document.getElementById("parkTypeList");
 const parkList = document.getElementById("parkList");
+const allList = document.getElementById("all");
 const parkInfoPara = document.getElementById("parkInfoPara");
 
 
-window.onload = function() {
+
+window.onload = function () {
     filterOptionsList.onchange = displayList;
     locationsList.onchange = parksByLocation;
     parkTypeList.onchange = parksByType;
     parkList.onchange = displayParkInfo;
+
 }
 
-// initializes the location list
 function initLocationsList() {
     locationsList.length = 0;
     parkInfoPara.style.display = "none"
 
-    let locationOption = new Option("Select a location", "select"); // creates select option for dropdown
-    locationsList.appendChild(locationOption); // adds "select" option to dropdown
+    let locationOption = new Option("Select a location", "select"); 
+    locationsList.appendChild(locationOption); 
 
     for (let location of locationsArray) {
         let option = new Option(location, location);
@@ -28,13 +30,12 @@ function initLocationsList() {
     }
 }
 
-// initializes the park type list
 function initParkTypeList() {
     parkTypeList.length = 0;
     parkInfoPara.style.display = "none"
 
-    let parkTypeOption = new Option("Select a park type", "select"); // creates select option for dropdown
-    parkTypeList.appendChild(parkTypeOption); // adds "select" option to dropdown
+    let parkTypeOption = new Option("Select a park type", "select");
+    parkTypeList.appendChild(parkTypeOption);
 
     for (let parkType of parkTypesArray) {
         let option = new Option(parkType, parkType);
@@ -42,7 +43,6 @@ function initParkTypeList() {
     }
 }
 
-// chooses which list to display based on which filter option is selected
 function displayList() {
     locationsList.style.display = "none";
     parkTypeList.style.display = "none";
@@ -63,21 +63,19 @@ function displayList() {
     }
 }
 
-// populates parkList based on location chosen
 function parksByLocation() {
     parkList.length = 0;
     parkInfoPara.style.display = "none"
 
-    let parkOption = new Option("Select a park", "select"); // creates select option for dropdown
-    parkList.appendChild(parkOption); // adds "select" option to dropdown
-
+    let parkOption = new Option("Select a park", "select"); 
+    parkList.appendChild(parkOption); 
     for (let park of nationalParksArray) {
         if (locationsList.value == park.State) {
             let option = new Option(park.LocationName, park.LocationID);
             parkList.appendChild(option);
         }
     }
-    if(locationsList.value != "select") {
+    if (locationsList.value != "select") {
         parkList.style.display = "block"
     }
     else {
@@ -86,13 +84,12 @@ function parksByLocation() {
     }
 }
 
-// populates parkList based on parkType chosen
 function parksByType() {
     parkList.length = 0;
     parkInfoPara.style.display = "none"
 
-    let parkOption = new Option("Select a park", "select"); // creates select option for dropdown
-    parkList.appendChild(parkOption); // adds "select" option to dropdown
+    let parkOption = new Option("Select a park", "select"); 
+    parkList.appendChild(parkOption); 
 
     for (let park of nationalParksArray) {
         if (park.LocationName.indexOf(parkTypeList.value) != -1) {
@@ -100,7 +97,7 @@ function parksByType() {
             parkList.appendChild(option);
         }
     }
-    if(parkTypeList.value != "select") {
+    if (parkTypeList.value != "select") {
         parkList.style.display = "block"
     }
     else {
@@ -109,19 +106,52 @@ function parksByType() {
     }
 }
 
-// displays park info when park is selected
 function displayParkInfo() {
     for (let park of nationalParksArray) {
         if (parkList.value == park.LocationID) {
-            parkInfoPara.innerHTML = "<span class='fw-bold'>Mailing Address: </span><br>" + park.LocationName + " (" + park.LocationID + ")  <br>" + park.Address + "<br>" + park.City + ", " + park.State + " " + park.ZipCode + "<br><span class='fw-bold'>Phone Number:</span> " + park.Phone + "<br><span class='fw-bold'>Fax Number:</span> " + park.Fax + "<br><span class='fw-bold'>Coordinates:</span> " + park.Latitude + ", " + park.Longitude;
+            parkInfoPara.innerHTML = "<span class='fw-bold'>Mailing Address: </span><br>"
+                + park.LocationName + " (" + park.LocationID + ")  <br>" + park.Address +
+                "<br>" + park.City + ", " + park.State + " " + park.ZipCode +
+                "<br><span class='fw-bold'>Phone Number:</span> " + park.Phone +
+                "<br><span class='fw-bold'>Fax Number:</span> " + park.Fax +
+                "<br><span class='fw-bold'>Coordinates:</span> " + park.Latitude +", " + park.Longitude;
+            if (park.Visit) {     
+            parkInfoPara.innerHTML += `<br><a href="${park.Visit}"> Visit </a>`
+            }
         }
     }
-    if(parkList.value != "select") {
+    if (parkList.value != "select") {
         parkInfoPara.style.display = "block"
     }
     else {
         parkInfoPara.style.display = "none"
     }
+
 }
 
-// consider using .visually-hidden class instead of style.display.none to hide and unhide elements for acessibilty purposes in the future
+function displayParks(parks) {
+
+    for (let park of parks) {
+        let tbodyOutput = document.getElementById("tbodyOutput")
+        let row = tbodyOutput.insertRow();
+        let tdParkName = row.insertCell();
+        tdParkName.innerHTML = park.LocationName;
+
+        let tdAddress = row.insertCell(1);
+        tdAddress.innerHTML = park.Address;
+
+        let tdCity = row.insertCell(2);
+        tdCity.innerHTML = park.City;
+
+        let tdState = row.insertCell(3);
+        tdState.innerHTML = park.State;
+
+        let tdZipCode = row.insertCell(4);
+        tdZipCode.innerHTML = park.ZipCode;
+    }};
+
+    function searchBtnOnClick() {
+        let all = document.getElementById("all");
+        let allValue = all.value;
+    }
+
